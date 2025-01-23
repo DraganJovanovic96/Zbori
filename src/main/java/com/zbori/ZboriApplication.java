c850e7031186307c5063dc031d14ba0c888e30d3
DESKTOP-I0S9ABL\davel <dragan.jovanovic.gale@gmail.com>
2025-01-23 17:23:28 +0000

    Initalize the project

---
+++ src/main/java/com/zbori/ZboriApplication.java
@@ -0,0 +1,13 @@
+package com.zbori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZboriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZboriApplication.class, args);
+	}
+
+}
